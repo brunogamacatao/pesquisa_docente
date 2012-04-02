6cf480c2b9ecf79e83ae1e6ce4e5782ed316602b
Bruno Gama Catão <brunogamacatao@gmail.com>
2012-04-02 10:15:13 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PesquisaDocente::Application.config.secret_token = '09194ae2ef453dac6492c403b4c1a504c988c56ddc54c6ed1b24313f259187c1e334e13ca5f84299cb9cdcd5b7ff79e44219a7b812912794b66998a1b157682f'
