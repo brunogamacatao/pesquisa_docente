171f60acf35be3e86e07b9d50ceec4810b1896f5
Bruno Gama Catão <brunogamacatao@gmail.com>
2012-04-04 11:23:40 -0300

    Voltei atras

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  after_filter :set_content_type
-  
-  protected
-  def set_content_type
-    headers['Content-Type'] ||= 'application/xhtml+xml; charset=utf-8'
-  end
 end
