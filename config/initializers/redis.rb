5c20dcba6c7a4333e3e3a6244575d7e109063784
Bruno Gama Catão <brunogamacatao@gmail.com>
2012-04-24 11:32:23 -0300

    Deixado a inicializacao do redis construir a cache apenas se necessario

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,20 +2,25 @@ $redis = Redis.new(:host => 'localhost', :port => 6379)
 
 puts "Construindo a cache"
 
-$redis.multi do
-  Turma.all.each do |turma|
-    turma.alunos.each do |aluno|
-      $redis.sadd(turma.redis_key(:alunos), aluno.id)
-      $redis.sadd(turma.disciplina.curso.redis_key(:alunos), aluno.id)
-      $redis.sadd(turma.disciplina.curso.instituicao.redis_key(:alunos), aluno.id)
+turma = Turma.first
+if $redis.scard(turma.redis_key(:alunos)) == 0
+  $redis.multi do
+    Turma.all.each do |turma|
+      turma.alunos.each do |aluno|
+        $redis.sadd(turma.redis_key(:alunos), aluno.id)
+        $redis.sadd(turma.disciplina.curso.redis_key(:alunos), aluno.id)
+        $redis.sadd(turma.disciplina.curso.instituicao.redis_key(:alunos), aluno.id)
     
-      unless aluno.respostas.empty? 
-        $redis.sadd(turma.redis_key(:alunos_responderam), aluno.id)
-        $redis.sadd(turma.disciplina.curso.redis_key(:alunos_responderam), aluno.id)
-        $redis.sadd(turma.disciplina.curso.instituicao.redis_key(:alunos_responderam), aluno.id)
+        unless aluno.respostas.empty? 
+          $redis.sadd(turma.redis_key(:alunos_responderam), aluno.id)
+          $redis.sadd(turma.disciplina.curso.redis_key(:alunos_responderam), aluno.id)
+          $redis.sadd(turma.disciplina.curso.instituicao.redis_key(:alunos_responderam), aluno.id)
+        end
       end
     end
   end
+else
+  puts "A cache ja estava criada"
 end
 
 puts "Pronto !"
