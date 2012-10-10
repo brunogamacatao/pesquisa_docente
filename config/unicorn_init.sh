69944f7c566f651d9c06561dc96f0f35919281d5
Bruno Gama Catão <brunogamacatao@gmail.com>
2012-10-10 11:35:56 -0300

    Changed default unicorn timeout settings

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Feel free to change any of the following variables for your app:
-TIMEOUT=${TIMEOUT-60}
+TIMEOUT=${TIMEOUT-600}
 APP_ROOT=/home/deployer/apps/pesquisa_docente/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
