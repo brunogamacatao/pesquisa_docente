046f3cd475878755fef002d2b00722cfbbaa3de7
Bruno Gama Catão <brunogamacatao@gmail.com>
2012-11-09 11:54:28 -0300

    added missing assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ PesquisaDocente::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( responder_pesquisa.js iniciar_respostas_unicas.js)
+  config.assets.precompile += %w(responder_pesquisa.js iniciar_respostas_unicas.js iniciar_respostas.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
